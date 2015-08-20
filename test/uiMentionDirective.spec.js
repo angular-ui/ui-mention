997db7c9b57098e993b5cd72a09662e97254a709
Kasper Lewau <lewauu@gmail.com>
2015-08-20 18:02:36 +0200

    feat(tests): unit all the tests
    * Split controller to separate file for test purposes.
    * Add gulp karma (single run)
    * Add gulp watch:karma (no single run)
    
    Closes #5

---
+++ test/uiMentionDirective.spec.js
@@ -0,0 +1,66 @@
+describe('uiMentionDirective', () => {
+  let Subject, $scope, compileDir, uiMentionController;
+
+  beforeEach(() => {
+    uiMentionController = function () {
+      uiMentionController.init = this.init = sinon.stub();
+    };
+
+    module('ui.mention', ($controllerProvider) => {
+      $controllerProvider.register('uiMentionController', uiMentionController);
+    });
+
+    inject(($injector) => {
+      Subject = $injector.get('uiMentionDirective');
+      $scope  = $injector.get('$rootScope').$new();
+
+      compileDir = (template) => {
+        return $injector.get('$compile')(template)($scope);
+      };
+    });
+  });
+
+  context('DDO', () => {
+    let DDO;
+
+    beforeEach(() => {
+      DDO = Subject[0];
+    });
+
+    it('is named uiMention', () => {
+      expect(DDO.name).to.eq('uiMention');
+    });
+
+    it('has a priority of 0', () => {
+      expect(DDO.priority).to.eq(0);
+    });
+
+    it('requires ngModel', () => {
+      expect(DDO.require).to.include('ngModel');
+    });
+
+    it('requires uiMention', () => {
+      expect(DDO.require).to.include('uiMention');
+    });
+
+    it('exposes controllerAs $mention', () => {
+      expect(DDO.controllerAs).to.eq('$mention');
+    });
+
+    it('is restricted to EA', () => {
+      expect(DDO.restrict).to.eq('EA');
+    });
+  });
+
+  context('.link()', () => {
+    it('calls the controller.init method with the given ngModel', () => {
+      $scope.model = 'wat';
+      compileDir('<span ui-mention ng-model="model"></span>');
+      $scope.$digest();
+      expect(uiMentionController.init).to.have.been.calledOnce.and.calledWithMatch({
+        $modelValue: 'wat'
+      });
+    });
+  });
+});
+
